406fd6e75919cd9d4bd46182fca3c40c3dc84823
Matthias.Renaud <renaud4ma@cl2n006.legi.grenoble-inp.fr>
2025-07-01 14:46:07 +0200

    correct test slopeAvalanche, check final bed position and slope, compute bed slope from qav components and compare with beta from solver

--- tests/slopeAvalanche/plotTest.py
+++ tests/slopeAvalanche/plotTest.py
@@ -1,5 +1,9 @@
 """
-
+Plot bed level at different times.
+Plot avalanche bedload flux at different times.
+Compute the slope angle at the final tim step with different methods.
+Using the bed level, using the bedload flux components.
+The bedload flux being tangent to the bed
 """
 
 import numpy as np
@@ -10,6 +14,10 @@ import os
 
 plt.rcParams["font.size"] = 15
 
+
+betaRepDeg = 32.  # repose angle in degrees
+betaRep = betaRepDeg * np.pi / 180
+Qav0 = 5e-3  # maximum bedload flux related to avalanche
 
 foamTimes = os.popen('foamListTimes').read()
 timeList = foamTimes.split('\n')[:-1]
@@ -23,48 +31,97 @@ cm = LinearSegmentedColormap.from_list(
         "Custom", colors, N=ntimes)
 colors = cm(np.arange(0, cm.N))
 
-maxBeta = np.zeros(ntimes)
-maxBetaPY = np.zeros(ntimes)
+maxBeta = np.zeros(ntimes)  # beta from solver
+maxBetaPY = np.zeros(ntimes)  # beta from qav components
+maxBetaZb = np.zeros(ntimes)  # qav from bed level
 
 
-fig, (axZb, axQb, axBeta) = plt.subplots(nrows=3, figsize=(8.3, 9))
+fig = plt.figure(figsize=(13, 8))
+
+gs = fig.add_gridspec(3, 3)
+
+axZb = fig.add_subplot(gs[0, 0])
+axQav = fig.add_subplot(gs[1, 0])
+axBeta = fig.add_subplot(gs[2, 0])
+axQavErr = fig.add_subplot(gs[1, 1])
+axBetaErr = fig.add_subplot(gs[2, 1])
+axMaxBeta = fig.add_subplot(gs[:, 2])
 
 for i, time in enumerate(timeList):
     Xbed, Ybed, Zbed = rdf.readmesh(
         "./", time, boundary="bed", verbose=False)
     axZb.plot(Xbed, Zbed, color=colors[i])
-    axZb.set_ylabel(r"$z_b\,[m]$")
+
+    betaOF = rdf.readscalar(
+        "./", time, "betaVf", boundary="bed", verbose=False)
+    axBeta.plot(Xbed, betaOF*180/np.pi, ls="solid", color=colors[i])
+    maxBeta[i] = np.max(betaOF)
 
     # avalanche bedload flux
     qavX, qavY, qavZ = rdf.readvector(
         "./", time, "qavVf", boundary="bed", verbose=False)
     magQav = np.sqrt(qavX**2 + qavZ**2)
+    axQav.plot(Xbed, magQav, color=colors[i])
 
-    axQb.plot(Xbed, magQav, color=colors[i])
-    axQb.set_ylabel(r"$q_{av}\,[m^2.s^{-1}]$")
+    qavVinent = Qav0 * (np.tanh(np.tan(betaOF)) - np.tanh(np.tan(betaRep)))
+    qavVinent /= (1 - np.tanh(np.tan(betaRep)))
 
-    betaOF = rdf.readscalar(
-        "./", time, "betaVf", boundary="bed", verbose=False)
-    maxBeta[i] = np.max(betaOF)
+    # relative error on avalanche related bedload
+    relErrQav = np.abs(magQav - qavVinent) / Qav0
+    axQavErr.plot(Xbed, relErrQav, color=colors[i])
 
     # compute beta from qav
     gradZb = np.arctan(qavZ/qavX)
-    betaPY = np.arctan(np.abs(gradZb))
+    betaPY = np.abs(gradZb)
+    axBeta.plot(Xbed, betaPY*180/np.pi, ls="dashed", color=colors[i])
     maxBetaPY[i] = np.max(betaPY)
 
+    relErrBeta = (betaPY - betaOF) / betaOF
+    axBetaErr.plot(Xbed, relErrBeta, color=colors[i])
 
-axBeta.plot(
-    timeArr, maxBeta * 180 / np.pi, color="#D55E00",
-    label="OpenFOAM")
-axBeta.plot(
+    # compute beta from bed level
+    maxBetaZb[i] = np.max(
+        np.arctan(
+            np.abs((Zbed[1:] - Zbed[:-1])/(Xbed[1:] - Xbed[:-1]))
+        )
+    )
+
+
+axZb.set_ylabel(r"$z_b\,[m]$")
+axZb.set_xticklabels([])
+axZb.grid()
+
+axQav.set_ylabel(r"$q_{av}\,[m^2.s^{-1}]$")
+axQav.set_xticklabels([])
+axQav.grid()
+
+axBeta.set_ylabel(r"$\beta$ in degrees")
+axBeta.set_xlabel("x [m]")
+axBeta.grid()
+
+axQavErr.set_ylabel(r"relative error on $q_{av}$")
+axQavErr.set_xlabel("x [m]")
+axQavErr.grid()
+
+axBetaErr.set_ylabel(r"relative error on $\beta$")
+axBetaErr.set_xlabel("x [m]")
+axBetaErr.grid()
+
+axMaxBeta.plot(
+    timeArr, maxBeta * 180 / np.pi, ls="solid",
+    color="#D55E00", label="OpenFOAM")
+axMaxBeta.plot(
     timeArr, maxBetaPY * 180 / np.pi, ls="dashed",
     color="#0072B2", label=r"from $q_{av}$")
+axMaxBeta.plot(
+    timeArr, maxBetaZb * 180 / np.pi, ls="dashdot",
+    color="#009E73", label=r"from bed level")
+axMaxBeta.axhline(betaRepDeg, color="black", ls="dashed", label="repose angle")
+axMaxBeta.set_ylabel(r"$max(\beta)$ in degrees")
+axMaxBeta.set_xlabel(r"time [s]")
+axMaxBeta.legend()
+axMaxBeta.grid()
 
-axQb.set_xlabel("x [m]")
-
-axBeta.set_ylabel(r"$max(\beta)$ in degrees")
-axBeta.set_xlabel(r"time [s]")
-axBeta.legend()
 
 fig.tight_layout()
 
--- tests/slopeAvalanche/test.py
+++ tests/slopeAvalanche/test.py
@@ -8,10 +8,12 @@ from fluidfoam import readof as rdf
 print(" --- running slope avalanche --- ")
 
 success = True
-tolBetaRep = 1e-3
-tolQav = 1e-4
+tolBetaRep = 1e-3  # tolerance with repose angle
+tolQav = 1e-4  # tolerance on bedload value, comparison with Vinent (2019)
+tolBeta = 1e-5  # error on beta from solver and computed from qav component
 
-time = "latestTime"
+time = "0.1"
+endTime = "latestTime"
 
 dS = 0.5e-3  # sediment diameter
 rhoS = 2650.  # sediment density
@@ -21,32 +23,38 @@ repAngle = 32.  # repose angle of granular material (in rad)
 
 # bed position
 Xbed, Ybed, Zbed = rdf.readmesh(
-    "./", "latestTime", boundary="bed", verbose=False)
+    "./", endTime, boundary="bed", verbose=False)
 
 tanBed = np.abs((Zbed[2:] - Zbed[:-2]) / (Xbed[2:] - Xbed[:-2]))
 
 maxAngle = (180/np.pi) * np.max(np.arctan(tanBed))
 
-relAngleError = (maxAngle - repAngle) / repAngle
-if relAngleError > tolBetaRep:
+repAngleError = (maxAngle - repAngle) / repAngle
+if repAngleError > tolBetaRep:
     success = False
     print(
         f"error! relative difference between repose angle "
         + f"({repAngle} deg) and maximum slope angle ({maxAngle} deg) "
-        + f"is {relAngleError*100} %\ntolerance is {tolBetaRep*100} %")
+        + f"is {repAngleError*100} %\ntolerance is {tolBetaRep*100} %")
 else:
-    print(f"bed slope OK, maximum slope is {round(maxAngle, 5)}°")
+    print(
+        f"bed slope OK, maximum slope is {round(maxAngle, 5)}°, "
+        + f"repose angle is {repAngle} °")
 
 # test value of avalanche
 Qav0 = 5e-3
 betaRep = repAngle * np.pi / 180
 qavX, qavY, qavZ = rdf.readvector(
-    "./", "0.1", "qavVf", boundary="bed", verbose=False)
+    "./", time, "qavVf", boundary="bed", verbose=False)
 magQav = np.sqrt(qavX**2 + qavZ**2)
-beta = np.arctan(np.abs(qavZ/qavX))
-qavVinent = Qav0 * (np.tanh(np.tan(beta)) - np.tanh(np.tan(betaRep)))
+# slope angle from sedExnerFOAM
+betaOF = rdf.readscalar("./", time, "betaVf", boundary="bed", verbose=False)
+# compute slope angle from qav components
+betaPY = np.arctan(np.abs(qavZ/qavX))
+qavVinent = Qav0 * (np.tanh(np.tan(betaPY)) - np.tanh(np.tan(betaRep)))
 qavVinent /= (1 - np.tanh(np.tan(betaRep)))
 
+# error on avalanche bedload flux mag
 relErrQav = np.max(np.abs(magQav - qavVinent) / np.max(qavVinent))
 if relErrQav > tolQav:
     success = False
@@ -54,7 +62,20 @@ if relErrQav > tolQav:
         f"ERROR! relative error on avalanche bedload flux value: "
         + f"{relErrQav*100} %\ntolerance is {tolQav*100} %")
 else:
-    print("avalanche flux OK")
+    print(f"avalanche flux OK, max error {relErrQav}")
+
+# error on slope angle from solver and qav components
+relErrBeta = (betaPY - betaOF) / betaOF
+if np.any(relErrBeta > tolBeta):
+    success = False
+    print(
+        "ERROR! difference between beta from solver and from qav component"
+        + f"\n  max(betaOF): {np.max(betaOF)*180/np.pi}°"
+        + f"\n  max(betaPY): {np.max(betaPY)*180/np.pi}°"
+        + f"\n  maximum relative error: {np.max(relErrBeta)*100}"
+        + f"%\ttolerance: {tolQav*100} %")
+else:
+    print(f"beta value OK, maximum relative error {np.max(relErrBeta)}")
 
 
 assert success
